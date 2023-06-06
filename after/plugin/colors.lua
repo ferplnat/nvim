791c0b7f0eed88cd5257e90d99b26f519e01b586
Marco Fernandez <marco.fernandez.99@gmail.com>
2023-06-06 12:02:43 -0700

    More commit

---
+++ after/plugin/colors.lua
@@ -0,0 +1,10 @@
+function ColorMyPencils(color)
+	color = color or "monokai-pro"
+	vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Visual", { bg = "#6b5000" })
+end
+
+ColorMyPencils()
