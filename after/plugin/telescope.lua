b07449531195e46b54ae2aeb88a1570d649002a0
Marco Fernandez <fernandezm@microsoft.com>
2023-09-18 10:51:49 -0700

    More update

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,14 @@
 local builtin = require('telescope.builtin')
+
+local function find_fallback()
+    if pcall(builtin.git_files) then
+        return
+    elseif pcall(builtin.find_files) then
+        return
+    end
+end
+
+vim.keymap.set('n', '<C-p>', find_fallback, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,3 +1,13 @@
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.powershell = {
+  install_info = {
+    url = "https://github.com/jrsconfitto/tree-sitter-powershell",
+    files = {"src/parser.c"}
+  },
+  filetype = "ps1",
+  used_by = { "psm1", "psd1", "pssc", "psxml", "cdxml" }
+}
+
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = "all",
