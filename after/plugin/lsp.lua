e540706c239c2223778c75b0b9462728f5e3c9c1
Marco Fernandez <marco.fernandez.99@gmail.com>
2023-09-26 17:13:52 -0700

    Add ghost text & misc adjustments

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -105,6 +105,10 @@ cmp.setup({
         end,
     },
 
+    experimental = {
+        ghost_text = true,
+    },
+
     window = {
         -- completion = cmp.config.window.bordered(),
         -- documentation = cmp.config.window.bordered(),
@@ -170,21 +174,31 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- See `:help vim.lsp.*` for documentation on any of the below functions
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end,
             { buffer = event.buf, desc = "[g]o to [d]efinition" })
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, { buffer = event.buf, desc = "Show hover" })
+
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end,
+            { buffer = event.buf, desc = "Show hover" })
+
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end,
             { buffer = event.buf, desc = "[v]iew [w]orkspace [s]ymbol" })
+
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end,
             { buffer = event.buf, desc = "[v]iew [d]iagnostic float" })
+
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end,
             { buffer = event.buf, desc = "next [d]iagnostic" })
+
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end,
             { buffer = event.buf, desc = "previous [d]iagnostic" })
+
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end,
             { buffer = event.buf, desc = "[v]iew [c]ode [a]ction" })
+
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end,
             { buffer = event.buf, desc = "[v]ariable [r]efe[r]ences" })
+
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end,
             { buffer = event.buf, desc = "[v]ariable [r]e[n]ame" })
+
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end,
             { buffer = event.buf, desc = "signature [h]elp" })
     end,
