3a17f079f5ea0564e66ebab2f8e3a459f2d3226a
Marco Fernandez <marco.fernandez.99@gmail.com>
2023-09-28 10:56:14 -0700

    Statusline updates: LSP and filetype status

--- lua/marco/statusline.lua
+++ lua/marco/statusline.lua
@@ -1,1 +1,29 @@
-vim.opt.statusline = "%<%f %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%) %P"
+vim.opt.statusline = '%!v:lua.require("marco.statusline").render()'
+
+local opts = {}
+local active_client_names = function()
+    local client_names = {}
+    local active_clients = vim.lsp.get_active_clients({ bufnr = vim.api.nvim_get_current_buf() })
+    for _, c in pairs(active_clients) do
+       client_names[#client_names + 1] = c.name
+    end
+
+    if #client_names == 0 then
+        return "no active lsp"
+    end
+
+    return string.format("active lsp: %s (%s)", table.concat(client_names, ', '), vim.bo.filetype)
+end
+
+function opts.render()
+    local statusline = {
+        "%<%f %h%m%r%{FugitiveStatusline()}",
+        " | "..active_client_names(),
+        "%=",
+        "%-14.(%l,%c%V%) %P ",
+    }
+
+    return table.concat(statusline, '')
+end
+
+return opts
